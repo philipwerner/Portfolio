8303d37166473a17f8863a84a8d492b8f667c239
Philip Werner <philip.r.werner@gmail.com>
2017-08-23 09:56:00 -0700

    added express, package.json and wrote server.js codez

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 //server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3333;
+app.use(express.static('./public'));
+app.use(function(req, res){
+  console.log('File not found, 404!!');
+  res.status(404).sendFile('404.html', {root: './public'});
+})
+app.listen(PORT, function(){
+  console.log('Portfolio is listening on port ' + PORT);
+})
