507fb18bbc078e41fb92674e099cc8d401926189
Philip Werner <philip.r.werner@gmail.com>
2017-08-30 15:37:57 -0700

    added proxy codez

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3333;
 const app = express();
 const conString = process.env.DATABASE_URL;
@@ -15,6 +16,15 @@ client.on('error', err => console.error(err));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+app.get('/github/*', proxyGitHub);
 
 app.get('/projects', (request, response) =>
 response.sendFile('index.html', {root: './public'}));
